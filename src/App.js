b1822715a2511842537cc8f477c163973494ff0a
Jose Ramón Vieites Romay <fabelaretorcida@gmail.com>
2020-09-13 14:26:54 +0200

    Adding todo items

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
+import Todo from './components/Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Masterfabela's Todo List</h1>
-      </header>
-      <Form></Form>
-    </div>
-  );
+	const [inputText, setInputText] = useState('');
+	const [todos, setTodos] = useState([]);
+	return (
+		<div className="App">
+			<header>
+				<h1>Masterfabela's Todo List</h1>
+			</header>
+			<Form
+				inputText={inputText}
+				todos={todos}
+				setTodos={setTodos}
+				setInputText={setInputText}
+			></Form>
+			<TodoList todos={todos}></TodoList>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,43 @@
 import React from 'react';
 
-function Form(){
-  return (
-    <form>
-      <input type="text" className="todo-input"/>
-      <button className="todo-button" type="submit">
-        <i className="fas fa-plus-square"></i>
-      </button>
-      <div className="select">
-        <select name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completad">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
-    </form>
-  );
+function Form({ setInputText, todos, setTodos, inputText }) {
+	const inputTextHandler = (event) => {
+		setInputText(event.target.value);
+	};
+	const submitTodoHandler = (event) => {
+		event.preventDefault();
+		if (inputText) {
+			setTodos([
+				...todos,
+				{ text: inputText, completed: false, id: Math.random() * 1000 },
+			]);
+			setInputText('');
+		}
+	};
+	return (
+		<form>
+			<input
+				onChange={inputTextHandler}
+				value={inputText}
+				type="text"
+				className="todo-input"
+			/>
+			<button
+				onClick={submitTodoHandler}
+				className="todo-button"
+				type="submit"
+			>
+				<i className="fas fa-plus-square"></i>
+			</button>
+			<div className="select">
+				<select name="todos" className="filter-todo">
+					<option value="all">All</option>
+					<option value="completad">Completed</option>
+					<option value="uncompleted">Uncompleted</option>
+				</select>
+			</div>
+		</form>
+	);
 }
 
 export default Form;
